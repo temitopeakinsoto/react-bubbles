8a5c2794ae7902b2642e32c9e2fc551d3ee80d27
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-18 07:53:39 +0100

    Create barebone login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useRef } from 'react';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <div>
+        username <input type="text" />
+        <br />
+        password <input type="text" />
+      </div>
+
+      <div>
+        <button>Submit</button>
+      </div>
+    </div>
   );
 };
 
